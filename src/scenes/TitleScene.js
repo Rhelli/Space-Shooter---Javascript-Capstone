a246b981f84e2d34f5f72af461539997459c2b79
Rory Hellier <rory.hellier@googlemail.com>
2020-11-16 17:23:07 +0000

    Added logic for the options button and added to the title scene. Changing branch to more apt name

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -11,6 +11,7 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create() {
+    // Game Button
     this.gameButton = this.add.sprite(100, 200, 'playButton').setInteractive();
     this.centerButton(this.gameButton, 1);
 
@@ -18,7 +19,6 @@ export default class TitleScene extends Phaser.Scene {
       this.scene.start('Game');
     });
 
-    // HOVER EFFECT FOR MENU BUTTONS
     this.input.on('pointerover', (event, gameObjects) => {
       gameObjects[0].setTexture('playButtonFocus');
     });
@@ -26,12 +26,20 @@ export default class TitleScene extends Phaser.Scene {
     this.input.on('pointerout', (event, gameObjects) => {
       gameObjects[0].setTexture('playButton');
     });
+
+    // Options button
+    this.optionsButton = this.add.sprite(300, 200, 'optionsButton').setInteractive();
+    this.centerButton(this.optionsButton);
+
+    this.optionsButton.on('pointerdown', (pointer) => {
+      this.scene.start('Options');
+    });
   }
 
   centerButton(gameObject, offset = 0) {
     Phaser.Display.Align.In.Center(
       gameObject,
-      this.add.zone(config.width / 2, config.height / 2 - offset * 100, config.width, config.height)
+      this.add.zone(config.width / 2, config.height / 2 - offset * - 100, config.width, config.height)
     );
   }
 }
