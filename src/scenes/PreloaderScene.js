90f6493d43f406721ec2299026dab8babb83ef9d
Rory Hellier <rory.hellier@googlemail.com>
2020-11-16 17:16:32 +0000

    Modified Title Scene Assets. Added Logic For Title Buttons:
     - Resized title buttons to smaller, precise size from full 500x500
     - Added create() logic to title to add the 'play' buttonto the title screen
     - Added logic for hover effects and clickthrough to the Game scene

--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -85,6 +85,7 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   ready() {
+    this.scene.start('Title');
     this.readyCount++;
     if (this.readyCount === 2) {
       this.scene.start('Title');
--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import config from '../config/config';
 
 export default class TitleScene extends Phaser.Scene {
   constructor() {
@@ -10,6 +11,27 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create() {
+    this.gameButton = this.add.sprite(100, 200, 'playButton').setInteractive();
+    this.centerButton(this.gameButton, 1);
 
+    this.gameButton.on('pointerdown', (pointer) => {
+      this.scene.start('Game');
+    });
+
+    // HOVER EFFECT FOR MENU BUTTONS
+    this.input.on('pointerover', (event, gameObjects) => {
+      gameObjects[0].setTexture('playButtonFocus');
+    });
+
+    this.input.on('pointerout', (event, gameObjects) => {
+      gameObjects[0].setTexture('playButton');
+    });
+  }
+
+  centerButton(gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center(
+      gameObject,
+      this.add.zone(config.width / 2, config.height / 2 - offset * 100, config.width, config.height)
+    );
   }
 }
