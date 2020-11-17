0b839ce81f03962084dbe1b91762ea7542ff13cf
Rory Hellier <rory.hellier@googlemail.com>
2020-11-17 23:02:19 +0000

    REMINDER:
     - Begin adding in each enemies individual laser color
     - Match enemy laser colors to their class
     - Continue loading in the rest of the game assets into Preloader()

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -5,17 +5,6 @@ export default class GameScene extends Phaser.Scene {
     super('Game');
   }
 
-  preload() {
-    // load Images Here
-    this.load.image('background0', './backgrounds/bkgd_0.png');
-    this.load.image('background1', './backgrounds/bkgd_1.png');
-    this.load.image('background2', './backgrounds/bkgd_2.png');
-    this.load.image('background3', './backgrounds/bkgd_3.png');
-    this.load.image('background4', './backgrounds/bkgd_4.png');
-    this.load.image('background6', './backgrounds/bkgd_6.png');
-    this.load.image('background7', './backgrounds/bkgd_7.png');
-  }
-
   create() {
     // Add images here
     this.add.image(400, 400, 'background0');
