8701e3c18186374f95af8e42891dd0ef32a86244
Rory Hellier <rory.hellier@googlemail.com>
2020-11-18 16:18:54 +0000

    Added more assets to the preloader:
     - Added the users ship and lasers
     - Added all enemy lasers and ships

--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -82,7 +82,7 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image('menuButton', './buttons/menuButton.png');
     this.load.image('menuButtonFocus', './buttons/menuButtonFocus.png');
 
-    // Game Scene Assets
+    // GAME SCENE ASSETS
     this.load.image('background0', './backgrounds/bkgd_0.png');
     this.load.image('background1', './backgrounds/bkgd_1.png');
     this.load.image('background2', './backgrounds/bkgd_2.png');
@@ -91,6 +91,7 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image('background6', './backgrounds/bkgd_6.png');
     this.load.image('background7', './backgrounds/bkgd_7.png');
 
+    // Explosion spritesheets
     this.load.spritesheet('explosion1', './explosions/explosion1.png', {
       frameWidth: 96,
       frameHeight: 96
@@ -103,6 +104,95 @@ export default class PreloaderScene extends Phaser.Scene {
       frameWidth: 128,
       frameHeight: 128
     });
+
+    // Enemy Ships & Lasers
+    // Ninja Ship
+    this.load.spritesheet('enemy1', './ships/Ninja.png', {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    // Ninja Laser
+    this.load.image('enemy1Lasers1', './lasers/blue/1');
+    this.load.image('enemy1Lasers2', './lasers/blue/2');
+    this.load.image('enemy1Lasers3', './lasers/blue/3');
+    this.load.image('enemy1Lasers4', './lasers/blue/4');
+    this.load.image('enemy1Lasers5', './lasers/blue/5');
+    this.load.image('enemy1Lasers6', './lasers/blue/6');
+    this.load.image('enemy1Lasers7', './lasers/blue/7');
+
+    //UFO Ship
+    this.load.spritesheet('enemy2', './ships/UFO.png', {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    // UFO Lasers
+    this.load.image('enemy1Lasers1', './lasers/red/1');
+    this.load.image('enemy1Lasers2', './lasers/red/2');
+    this.load.image('enemy1Lasers3', './lasers/red/3');
+    this.load.image('enemy1Lasers4', './lasers/red/4');
+    this.load.image('enemy1Lasers5', './lasers/red/5');
+    this.load.image('enemy1Lasers6', './lasers/red/6');
+    this.load.image('enemy1Lasers7', './lasers/red/7');
+
+    // Paranoid Ship
+    this.load.spritesheet('enemy3', './ships/Paranoid.png', {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    // Paranoid Lasers
+    this.load.image('enemy3Lasers1', './lasers/green/1');
+    this.load.image('enemy3Lasers2', './lasers/green/2');
+    this.load.image('enemy3Lasers3', './lasers/green/3');
+    this.load.image('enemy3Lasers4', './lasers/green/4');
+    this.load.image('enemy3Lasers5', './lasers/green/5');
+    this.load.image('enemy3Lasers6', './lasers/green/6');
+
+    // Saboteur Ship
+    this.load.spritesheet('enemy4', './ships/Saboteur.png', {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    // Saboteur Lasers
+    this.load.image('enemy4Lasers1', './lasers/pink/1');
+    this.load.image('enemy4Lasers2', './lasers/pink/2');
+    this.load.image('enemy4Lasers3', './lasers/pink/3');
+    this.load.image('enemy4Lasers4', './lasers/pink/4');
+    this.load.image('enemy4Lasers5', './lasers/pink/5');
+    this.load.image('enemy4Lasers6', './lasers/pink/6');
+    this.load.image('enemy4Lasers7', './lasers/pink/7');
+
+    // Lightning Ship
+    this.load.spritesheet('enemy5', './ships/Lightning.png', {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    // Lightning Lasers
+    this.load.image('enemy5Lasers1', './lasers/yellow/1');
+    this.load.image('enemy5Lasers2', './lasers/yellow/2');
+    this.load.image('enemy5Lasers3', './lasers/yellow/3');
+    this.load.image('enemy5Lasers4', './lasers/yellow/4');
+    this.load.image('enemy5Lasers5', './lasers/yellow/5');
+    this.load.image('enemy5Lasers6', './lasers/yellow/6');
+    this.load.image('enemy5Lasers7', './lasers/yellow/7');
+    this.load.image('enemy5Lasers8', './lasers/yellow/8');
+    this.load.image('enemy5Lasers9', './lasers/yellow/9');
+
+    // Player Ship
+    this.load.spritesheet('playerShip', './ships/Lighter.png', {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+
+    // Player lasers
+    this.load.image('playerLaser1', './lasers/turq/1');
+    this.load.image('playerLaser2', './lasers/turq/2');
+    this.load.image('playerLaser3', './lasers/turq/3');
+    this.load.image('playerLaser4', './lasers/turq/4');
+    this.load.image('playerLaser5', './lasers/turq/5');
+    this.load.image('playerLaser6', './lasers/turq/6');
+    this.load.image('playerLaser7', './lasers/turq/7');
+    this.load.image('playerLaser8', './lasers/turq/8');
+    this.load.image('playerLaser9', './lasers/turq/9');
 
     this.load.audio('titleMusic', ['./music/titleMusic.ogg']);
   }
