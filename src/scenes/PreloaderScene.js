d3f3571060b90c3687cad97e989d1419bc8c821e
Rory Hellier <rory.hellier@googlemail.com>
2020-11-26 16:20:07 +0000

    Updated asset folder names as they were unecceserily capitalised

--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -216,7 +216,6 @@ export default class PreloaderScene extends Phaser.Scene {
 
   ready() {
     this.readyCount++;
-    //this.scene.start('PostGameHighscoresScene');
     if (this.readyCount === 2) {
       this.cameras.main.fadeOut(1000, 0, 0, 0);
       this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
